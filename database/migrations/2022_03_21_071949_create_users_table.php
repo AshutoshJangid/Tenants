c490c0ce64be99c54f4f03fca7d9b52723564367
t111 <t111@sagdomain.local>
2022-03-21 18:44:56 +0530

    Project structure is created

--- database/migrations/2022_03_21_071949_create_users_table.php
+++ database/migrations/2022_03_21_071949_create_users_table.php
@@ -17,9 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('image');
+            $table->string('type');
+            $table->enum('status',['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
